d76002a65059401558a5f211a47c3f7598458fd9
Diego Silva <social.diegosilvatech@gmail.com>
2022-09-27 20:33:43 -0300

    chore: test lintstaged

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,10 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import React from 'react';
 
+const name = 'diego';
+
+console.log(name);
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
